b5f9fe641f5d3eb3ac85c32f1b8b23e20fa80f12
Gildas Cuisinier <gcuisinier@gcuisinier.local>
2014-10-24 06:30:09 +0200

    refresh

--- jenv.rb
+++ jenv.rb
@@ -1,11 +1,10 @@
 require 'formula'
 
-
 class Jenv < Formula
-  homepage 'http://www.jenv.be'
-  url 'https://github.com/gcuisinier/jenv/archive/0.4.1.tar.gz'
-  sha1 'e0615726e27a1c4f6a39f997949b4610dd9954df'
-  head 'https://github.com/gcuisinier/jenv.git', :branch => 'master'
+  homepage "http://www.jenv.be"
+  url "https://github.com/gcuisinier/jenv/archive/0.4.1.tar.gz"
+  sha1 "e0615726e27a1c4f6a39f997949b4610dd9954df"
+  head "https://github.com/gcuisinier/jenv.git"
 
   def install
      libexec.install Dir['*']
@@ -22,9 +21,8 @@ class Jenv < Formula
    end
 
   test do
-    (testpath/'.java-version').write <<-EOS.undent
-      homebrew-test
-    EOS
-    system jenv version 2>&1 | grep "jenv: version" | grep "homebrew" | grep "is not installed"
+    (testpath/".java-version").write "homebrew-test"
+    output = `jenv version 2>&1`
+    assert output.include? "jenv: version `homebrew-test' is not installed"
   end
 end
